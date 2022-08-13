acade6c0cb06983260b9cf7bb33e29097ce311e1
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-08-12 21:22:54 -0700

    Cleaned up the files and added new features
    Co-authored-by: Evan Ugarte <evanugarte@users.noreply.github.com>

--- alerting/ping.js
+++ alerting/ping.js
@@ -1,96 +1,71 @@
-const { logger } = require('../util/logger');
 const exec = require('exec');
+
 const client = require('prom-client');
 const express = require('express');
+
+const logger = require('../util/logger');
+const {
+  devices,
+  scrapeIntervalSeconds,
+  metricsPort,
+  packetCount,
+} = require('../config/config.json');
+
+logger.info('Starting ping scraper with options', {
+  scrapeIntervalSeconds,
+  metricsPort,
+  packetCount,
+});
+
 const app = express();
 let register = new client.Registry();
 
-const rawArgs = process.argv.slice(2);
-let IPStemp = [];
-let names= [];
-
-for (let i = 0; i < rawArgs.length; i += 2) {
-    switch (rawArgs[i]) {
-      case '--ips':
-        IPStemp[0] = rawArgs[i + 1].replace(/["']/g, '');
-        if(rawArgs[i + 1].includes(','))
-        {
-          for(let j  = 0; j < rawArgs[i + 1].split(',').length; j++)
-          {
-            IPStemp[j] = rawArgs[i + 1].split(',')[j].replace(/["']/g, '');
-          }
-        }
-        break;
-      case '--names':
-        names[0] = rawArgs[i + 1].replace(/["']/g, '');
-        if(rawArgs[i + 1].includes(','))
-        {
-          for(let j  = 0; j < rawArgs[i + 1].split(',').length; j++)
-          {
-            names[j] = rawArgs[i + 1].split(',')[j].replace(/["']/g, '');
-          }
-        }
-        break;
-      default:
-        return "ping.js --ips <ips> --names<names>";
-    }
-  }
-
-let IPs = Object.assign.apply({}, names.map( (v, i) => ( {[v]: IPStemp[i]} ) ) );
-
-const akshit = new client.Gauge({
-    name: "DeviceDown",
-    help: "device is down",
+const pingingMetric = new client.Gauge({
+    name: "device_last_seen",
+    help: "The last time a device was seen. Is set to the current time a device was seen",
     labelNames: ['ipAddress', 'deviceName'],
 });
 
-register.registerMetric(akshit);
-
-function pingIP(ip,name) {
-  let timer = Date.now()
-    return new Promise((resolve) => {
-        try{
-            exec(`ping -c 5 ${ip}`, (err, stdout, stderr) => {
-                if(err){
-                    console.log(`adding ${Math.floor((Date.now() - timer) / 1000)} for `, ip)
-                    akshit.labels(ip, name).inc(Math.floor((Date.now() - timer) / 1000))
-                    resolve(false);
-                }
-                if(stderr){
-                    console.log(`adding ${Math.floor((Date.now() - timer) / 1000)} for `, ip)
-                    akshit.labels(ip, name).inc(Math.floor((Date.now() - timer) / 1000))
-                    resolve(false);
-                } 
-                else if (stdout){                  
-                    console.log("setting to 1 for ", ip)
-                    akshit.labels(ip, name).set(0)
-                    resolve(true);
-                }
-            });
-        } catch(err){
-            console.log('function had an error: ', error);
-            resolve(false);
-        }
-    });
-
-}
-
+register.registerMetric(pingingMetric);
 register.setDefaultLabels ({
-    app: 'ping-temp'
+  app: 'ping'
 });
 client.collectDefaultMetrics({ register });
 
+function executePingCommand(ip) {
+  return new Promise((resolve) => {
+    exec(`ping -c ${packetCount} ${ip}`, (error, stdout, stderr) => {
+      if(error || stderr) {
+        logger.error(`Unable to ping ${ip}: \n`, {error, stderr, stdout})
+        return resolve(false);
+      }
+      return resolve(true);
+    });
+  });
+}
+
+async function checkIfDeviceUp(ip, name) {
+  const result = await executePingCommand(ip);
+  if (result) {
+    pingingMetric.labels(ip, name).set(Date.now())
+  }
+}
+  
 app.get('/metrics', async (request, response) => {
     response.setHeader('Content-Type', register.contentType);
     response.end(await register.metrics());
 });
   
-app.listen(5000, () =>{
-    console.log('Started server on port 5000');
+app.listen(metricsPort, () =>{
+    logger.info(`Started server on port ${metricsPort}`);
+});
+
+devices.forEach(({ip, name}) => {
+  pingingMetric.labels(ip, name).set(0)
 });
 
 setInterval(async () => {
-    Object.keys(IPs).forEach(key => {
-        pingIP(IPs[key],key)
-    })
-}, 10000);
+    devices.forEach(({ip, name}) => {
+      checkIfDeviceUp(ip, name);
+  });
+}, scrapeIntervalSeconds * 1000);
